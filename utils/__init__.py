401867a8e3968a1d270adc7e9175ddacff8d4051
77iSac77 <gabrielisac@icloud.com>
2026-01-30 00:32:35 -0300

    Criar __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+"""OpusDeiTradeMetaL - Utilidades."""
+from .time_utils import (
+    format_timestamp_all_zones,
+    format_price,
+    format_percent,
+    format_large_number,
+)
+from .llm_client import get_llm_client
