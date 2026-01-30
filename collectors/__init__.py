1ef08a2232d7694dcb22fedcd3c9f2a4c64b9518
77iSac77 <gabrielisac@icloud.com>
2026-01-30 00:40:18 -0300

    Criar __init__.py

---
+++ collectors/__init__.py
@@ -0,0 +1,5 @@
+"""OpusDeiTradeMetaL - Coletores de dados."""
+from .prices import get_price_collector
+from .technical import get_technical_analyzer
+from .macro import get_macro_collector
+from .institutional import get_institutional_collector
