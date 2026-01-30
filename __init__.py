c01387a4c1ec4e39e9b94fe337579b36d07aaa5d
77iSac77 <gabrielisac@icloud.com>
2026-01-30 00:19:31 -0300

    Excluir __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""OpusDeiTradeMetaL - Coletores de dados."""
-from .prices import get_price_collector
-from .technical import get_technical_analyzer
-from .macro import get_macro_collector
-from .institutional import get_institutional_collector
